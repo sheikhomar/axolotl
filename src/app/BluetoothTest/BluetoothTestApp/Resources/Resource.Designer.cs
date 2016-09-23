e8937988a0f92e1aefcbdbfae603b28cf3af31d8
MarcBeuchert <marcbeuchert@hotmail.com>
2016-09-23 11:21:57 +0200

    Made changes to bluetooth system

--- src/app/BluetoothTest/BluetoothTestApp/Resources/Resource.Designer.cs
+++ src/app/BluetoothTest/BluetoothTestApp/Resources/Resource.Designer.cs
@@ -60,8 +60,17 @@ namespace BluetoothTestApp
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int ConnectButton = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int MessageText = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int ResultText = 2131034114;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int SearchButton = 2131034112;
 			
 			static Id()
 			{
