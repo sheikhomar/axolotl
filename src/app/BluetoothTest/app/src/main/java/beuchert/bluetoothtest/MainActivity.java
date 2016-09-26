25d829f297b272c567c56dd16676ae8d0cc0386b
MarcBeuchert <marcbeuchert@hotmail.com>
2016-09-26 09:58:04 +0200

    Added some change for Omar

--- src/app/BluetoothTest/app/src/main/java/beuchert/bluetoothtest/MainActivity.java
+++ src/app/BluetoothTest/app/src/main/java/beuchert/bluetoothtest/MainActivity.java
@@ -3,10 +3,13 @@ package beuchert.bluetoothtest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -27,13 +30,14 @@ public class MainActivity extends AppCompatActivity {
         Button disconnectButton = (Button) findViewById(R.id.DisconnectButton);
     }
 
+    private String MacAdress = "00:0C:78:33:A5:63";
     public void connectClick(View view) throws IOException {
         BluetoothAdapter BA = null;
         BluetoothSocket mSocket = null;
         final UUID MY_UUID = UUID.fromString("c3ffbcc2-ab89-4e56-94ed-2a8df65e45bd");
 
         BA = BluetoothAdapter.getDefaultAdapter();
-        BluetoothDevice device = BA.getRemoteDevice("EC:55:F9:FD:AF:BB");
+        BluetoothDevice device = BA.getRemoteDevice(MacAdress);
 
         mSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
         mSocket.connect();
@@ -51,5 +55,11 @@ public class MainActivity extends AppCompatActivity {
                 mSocket.close();
             }
         }
+    }
+
+    public void ChangeOnClick(View view){
+        EditText MacAdressEditText = (EditText) findViewById(R.id.NewMacText);
+        Editable NewMacAdress = MacAdressEditText.getText();
+        MacAdress = NewMacAdress.toString();
     }
 }
