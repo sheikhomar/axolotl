eaaa04b9588e29ff0b48dd77d4703e000e8dc4f2
MarcBeuchert <marcbeuchert@hotmail.com>
2016-09-27 10:26:48 +0200

    Android: Added methods to bluetooth controller interface

--- src/app/BluetoothTest/app/src/main/java/beuchert/bluetoothtest/Interfaces/IBluetoothController.java
+++ src/app/BluetoothTest/app/src/main/java/beuchert/bluetoothtest/Interfaces/IBluetoothController.java
@@ -1,4 +1,10 @@
 package beuchert.bluetoothtest.Interfaces;
 
+import java.util.UUID;
+
 public interface IBluetoothController {
+    public void Connect();
+    public void Write();
+    public void Read();
+    public void Disconnect();
 }
