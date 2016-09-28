b7fe4221c8388726b5ec1d177300d8aa360b74b1
MarcBeuchert <marcbeuchert@hotmail.com>
2016-09-28 12:57:07 +0200

    Added a connected thread for bluetooth

---
+++ src/app/BluetoothTest/app/src/main/java/beuchert/bluetoothtest/Helpers/ConnectedThread.java
@@ -0,0 +1,16 @@
+package beuchert.bluetoothtest.Helpers;
+
+/**
+ * Created by marcb on 28-09-2016.
+ */
+
+public class ConnectedThread extends Thread {
+
+    public void ConnectedThread(){
+
+    }
+
+    public void Run(){
+
+    }
+}
