59864390bc69bb1f19e9f31544ef7b1eba7ee012
MarcBeuchert <marcbeuchert@hotmail.com>
2016-10-18 15:06:58 +0200

    Changes to drawingView, now it can draw the bricks

--- src/app/BluetoothTest/app/src/main/java/beuchert/bluetoothtest/Views/DrawingView.java
+++ src/app/BluetoothTest/app/src/main/java/beuchert/bluetoothtest/Views/DrawingView.java
@@ -1,6 +1,10 @@
 package beuchert.bluetoothtest.Views;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -10,6 +14,13 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import beuchert.bluetoothtest.Activities.MainActivity;
+import beuchert.bluetoothtest.Models.Layer;
+import beuchert.bluetoothtest.Models.Package;
+import beuchert.bluetoothtest.R;
 
 public class DrawingView extends View {
     int i = 0;
@@ -18,69 +29,102 @@ public class DrawingView extends View {
     Rect bounds;
     Paint borderPaint ;
     Random random ;
-    int width , height;
+    MainActivity mainActivity;
+    int width , length;
+    List<Layer> layers;
+    int selectedLayer = 0;
 
-    public DrawingView(Context context, int width, int height) {
+    public DrawingView(Context context, int length, int width) {
         super(context);
 
+        mainActivity = (MainActivity) context;
+        this.length = length;
         this.width = width;
-        this.height = height;
 
-        frame = Bitmap.createBitmap(width,height,Bitmap.Config.ARGB_8888);
+        layers = new ArrayList<Layer>();
+
+        frame = Bitmap.createBitmap(width,length,Bitmap.Config.ARGB_8888);
         frameDrawer = new Canvas(frame);
-        bounds = new Rect(0 , 0, width,height);
-        //this initialization will make the frameDrawer draw on the frame bitmap
+        bounds = new Rect(0 , 0, width,length);
 
-        //always avoid allocating new objects in the draw method to optimize the performance 
         borderPaint = new Paint();
         random = new Random();
         borderPaint.setStyle(Paint.Style.STROKE);
         borderPaint.setColor(Color.BLACK);
-        Rect r = new Rect(0, 0, width-1, height-1);
-        frameDrawer.drawRect(r, borderPaint);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        borderPaint.setColor(Color.argb(255, random.nextInt(255),
-                random.nextInt(255), random.nextInt(255)));
-        //instead of drawing on the view canvas draw on the frame bitmap
-        //frameDrawer.drawLine(random.nextInt(width), random.nextInt(height),
-        //        random.nextInt(width), random.nextInt(height), paint);
-        // and then draw the bitmap on the view canvas
-        canvas.drawBitmap(frame, null, bounds , null);
+        Rect r = new Rect(0, 0, width-1, length-1);
+        frameDrawer.drawRect(r, borderPaint);
 
-        //if (i < 1000) {
-        //    i++;
-        //    invalidate();
-        //}
+        canvas.drawBitmap(frame, null, bounds , null);
     }
 
-    public void drawRectangle(int x, int y, int length, int width, int paintColor){
-        Rect rBorder = new Rect(x, y, x+length, y+width);
-        frameDrawer.drawRect(rBorder,borderPaint);
+    public void addPackage(Package pack){
+        Layer layer = null;
 
-        Paint color = new Paint();
-        borderPaint.setStyle(Paint.Style.FILL);
-
-        switch(paintColor){
-            case 0:
-                color.setColor(Color.RED);
-                break;
-            case 1:
-                color.setColor(Color.GREEN);
-                break;
-            case 2:
-                color.setColor(Color.BLUE);
-                break;
-            case 3:
-                color.setColor(Color.YELLOW);
-                break;
+        if (layers.size()-1 < pack.layer){
+            while(layers.size()-1 < pack.layer){
+                layers.add(new Layer());
+                if (layers.size()-1 == pack.layer){
+                    layer = layers.get(pack.layer);
+                    mainActivity.addElementToSpinner("Layer " + pack.layer);
+                }
+            }
         }
-        Rect rFill = new Rect(x+1, y+1, x+length-1, y+width-1);
-        frameDrawer.drawRect(rFill,color);
+        else
+            layer = layers.get(pack.layer);
+
+        layer.addPackageToLayer(pack);
+
+        selectLayer(pack.layer);
+
+        redrawSelectedLayer();
+    }
+
+    private void redrawSelectedLayer(){
+        frameDrawer.drawColor(Color.WHITE);
+
+        Rect r = new Rect(0, 0, width-1, length-1);
+        frameDrawer.drawRect(r, borderPaint);
+
+        Layer layer = layers.get(selectedLayer);
+        List<Package> packList = layer.Packages;
+
+        for (Package pack:packList) {
+            Rect rBorder = new Rect(pack.x, pack.y, pack.x+pack.length, pack.y+pack.width);
+            frameDrawer.drawRect(rBorder,borderPaint);
+
+            Rect rFill = new Rect(pack.x+1, pack.y+1, pack.x+pack.length, pack.y+pack.width);
+            frameDrawer.drawRect(rFill,pack.paint);
+        }
+
+        frameDrawer.drawBitmap(frame, null, bounds , null);
 
         invalidate();
-
     }
+
+    public void selectLayer(int selector){
+        selectedLayer = selector;
+        mainActivity.setSelectedElementInSpinner(selector-1);
+        redrawSelectedLayer();
+    }
+
+    public void setSize(int length, int width){
+        frameDrawer.drawColor(Color.WHITE);
+        this.length = length;
+        this.width = width;
+
+        frame = Bitmap.createBitmap(width,length,Bitmap.Config.ARGB_8888);
+        frameDrawer = new Canvas(frame);
+        bounds = new Rect(0 , 0, width,length);
+
+        Rect r = new Rect(0, 0, width-1, length-1);
+        frameDrawer.drawRect(r, borderPaint);
+
+        invalidate();
+        redrawSelectedLayer();
+    }
+
 }
