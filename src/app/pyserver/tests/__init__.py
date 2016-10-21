ebf92e8c3dc0a354e0200bf4c8fc2af0d02dc554
Frederik Kirk Kristensen <Frederik_KKristensen@hotmail.com>
2016-10-21 10:58:44 +0200

    BP: Added unit test for gravity_check

--- src/app/pyserver/tests/__init__.py
+++ src/app/pyserver/tests/__init__.py
@@ -3,3 +3,4 @@ from .packing_advisor_handle_tests import *
 from .layer_find_occupying_package_tests import *
 from .layer_calc_fill_level_tests import *
 from .layer_pack_tests import *
+from .packing_advisor_gravity_check_test import *
---
+++ src/app/pyserver/tests/packing_advisor_gravity_check_test.py
@@ -0,0 +1,32 @@
+import unittest
+
+from lib import Bin
+from lib import Package
+from lib import PackingAdvisor
+
+class PackingAdvisorGravityCheckTest(unittest.TestCase):
+    
+    def test_gravity_check(self):
+        bin1 = Bin(4,4,3)
+        pa = PackingAdvisor(bins = [bin1])
+        p1 = Package(2, 2)
+        p2 = Package(4, 2)
+        p3 = Package(4, 2)
+        p4 = Package(4, 2)
+
+        pa.handle(p1)
+        pa.handle(p2)
+
+        pa.bins[0].new_layer()
+        pa.x = 0
+        pa.y = 0
+        self.assertFalse(pa.check_gravity(pa.bins[0].current_layer, p3) > 0.75)
+        pa.handle(p3)
+
+        pa.bins[0].new_layer()
+        pa.x = 2
+        pa.y = 0
+        self.assertTrue(pa.check_gravity(pa.bins[0].current_layer, p4) > 0.75)
+        
+
+
