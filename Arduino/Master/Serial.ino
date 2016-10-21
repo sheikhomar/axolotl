c03a80fa6d838a2ff3643b50096ca37d01338ccd
MBS <mathiasbsvendsen@gmail.com>
2016-10-21 08:08:47 +0200

    Cleaned Arduino Serial Comm

--- Arduino/Master/Serial.ino
+++ Arduino/Master/Serial.ino
@@ -1,15 +1,71 @@
-void SerialCountTest()
-{
+void serialSendData(client reciver, byte data[], byte sizeOfData, byte reciverFunction) {
+	int i = 0;
+
+	//if() check that data is nonempty, if data fits buffer...
+
+	//handshake
+
+
+	digitalWrite(SerialTransmitPin, HIGH);
+
+	Serial.write(reciver);
+	Serial.write(sizeOfData + 1);
+	Serial.write(reciverFunction);
+
+	for (i = 0; i < sizeOfData; i++) {
+		Serial.write(data[i]);
+		//delay(10);
+	}
+	digitalWrite(SerialTransmitPin, LOW);
+}
+
+
+void serialReadData(bool readAll) {
+	byte id, length, command, data[serialMaxDataSize];
 	int i;
 
-	digitalWrite(SerialTransmitPin, RS485Transmit);  // Enable RS485 Transmit   
+	//read id and length
+	id = Serial.read();
+	length = Serial.read();
 
-	for (i = 'a'; i< 'a' + 26; i++) {
-		Serial.write((byte)i);          // Send byte to Remote Rs485 devices
-		delay(10);
+	if (id == -1)
+		return;
+
+	if (id != Arduino) {
+		for (i = 0; i < length; i++)
+		{
+			Serial.read();
+		}
+		return;
 	}
 
-	digitalWrite(SerialTransmitPin, RS485Receive);  // Disable RS485 Transmit         
+	//save Arduino message
+	command = Serial.read();
+	for (i = 0; i < length - 1; i++)
+	{
+		data[i] = Serial.read();
+	}
 
-	while (true);
+	switch (command)
+	{
+	case 0:
+		//do stuff
+		break;
+	default:
+		break;
+	}
+}
+
+
+
+void serialCountTest()
+{
+	int i, sizeOfData = 26;
+	byte myArr[sizeOfData];
+
+	for (i = 0; i<sizeOfData; i++) {
+		myArr[i] = 'a' + i;
+	}
+
+	serialSendData(NXT, myArr, sizeOfData, 0);
 }
