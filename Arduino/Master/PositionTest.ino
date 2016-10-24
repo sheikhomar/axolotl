eca7d3840775ef20ce16a5c494a12588f5f5ba8d
stelle <stellehj@yahoo.dk>
2016-10-24 08:55:24 +0200

    Trying to fix PositionTest

--- Arduino/Master/PositionTest.ino
+++ Arduino/Master/PositionTest.ino
@@ -5,10 +5,11 @@ void determineTimeBetweenSensors() {
 	byte data[] = { 'T','r','a','v','e','l','t','i','m','e',':',' ' };
 	byte data2[32];
 
+serialDebug("Her ");
 	while (!sensorRead) {
 		sensorRead = tag();
 	}
-
+serialDebug("Her1");
 	iniTime = millis();
 	sensorRead = false;
 
@@ -19,19 +20,21 @@ void determineTimeBetweenSensors() {
 	timeColour = millis();
 	timeColour = timeColour - iniTime;
 	travelTime = (double)timeColour;
-	//SerialSendData(Debug, data);
-	//SerialSendData(travelTime);
+	serialDebug("Traveltime: ");
+	serialDebug((String)timeColour);
 	return;
 }
 
 bool colourSensorReady() {
 	bool ready = false;
-	byte dataS[] = { 50 };
+	byte dataS[] = {5};
+  byte dataRec[1];
 	int colour = -1;
 	serialSendData(NXT, dataS, 0, 5);
 
 	while (colour != 0 || colour != 1 || colour != 2 || colour != 3 || colour != 6 || colour != 7) {
-		//colour = serialReadData();
+		serialReadData(dataRec, 1);
+    colour = dataRec[0];
 	}
 	return true;
 }
