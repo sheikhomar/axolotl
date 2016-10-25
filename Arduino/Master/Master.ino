1f4cc193e3e1530e154dbaebe5c60f88daaeb62f
Omar Ali Sheikh-Omar <sheikhomar@gmail.com>
2016-10-25 14:24:16 +0200

    Implement die function :tada:

--- Arduino/Master/Master.ino
+++ Arduino/Master/Master.ino
@@ -155,6 +155,11 @@ void simplifiedDemo() {
   }
 }
 
+void die(String abortMessage) {
+  serialDebug(abortMessage);
+  while(1);
+}
+
 void runConveyorBeltAtSpeed(byte speed) {
    byte data[1] = { speed };
    serialSendData(NXT, data, 1, 3);
