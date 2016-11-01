e4313930fec4be44f49c1b1f5aa7d4213ea8954b
MBS <mathiasbsvendsen@gmail.com>
2016-11-01 10:03:50 +0100

    Moved UltTest to a separate file

--- src/app/ArduinoMaster/UltrasoundSensor.cpp
+++ src/app/ArduinoMaster/UltrasoundSensor.cpp
@@ -39,37 +39,3 @@ unsigned short GetUltDistance(int trigPin, int echoPin, bool wait) {
 
 	return (unsigned short)(duration*ULT_MM_PER_US * 100);
 }
-
-
-
-/***************************
-TestUltSensors
-
-Prints the measured distance of all UltSensors and whether they will TAG.
-***************************/
-void TestUltSensors() {
-	unsigned short distance1, distance2, distance3;
-
-	distance1 = GetUltDistance(ULT1_TRIG_PIN, ULT1_ECHO_PIN, false);
-	//delay?
-	distance2 = GetUltDistance(ULT2_TRIG_PIN, ULT2_ECHO_PIN, false);
-	distance3 = GetUltDistance(ULT3_TRIG_PIN, ULT3_ECHO_PIN, false);
-
-	//Create debug string
-	String masterString;
-	masterString.concat(String(distance1));
-	masterString.concat("\t");
-	masterString.concat(String(distance2));
-	masterString.concat("\t");
-	masterString.concat(String(distance3));
-	masterString.concat("\t");
-
-	masterString.concat(String(distance1 < ult1_TagDist));
-	masterString.concat("  ");
-	masterString.concat(String(distance2 < ult2_TagDist));
-	masterString.concat("  ");
-	masterString.concat(String(distance3 < ult3_TagDist));
-	serialDebugLN(masterString);
-
-	delay(ULT_MEASUREMENT_CYCLE_MS);
-}
