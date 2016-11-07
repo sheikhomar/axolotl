f8b11abf882709a0a17086ae9b441c018966d2d5
Frederik Kirk Kristensen <Frederik_KKristensen@hotmail.com>
2016-11-07 15:39:35 +0100

    PIComms: BigPythonMain updated for Big test :cactus:

--- BigPythonMain.py
+++ BigPythonMain.py
@@ -7,8 +7,72 @@ from lib import *
 
 
 ####### Serial communication instance    #########
-ser = serial.Serial('/dev/ttyACM0',9600)
+ser = serial.Serial('/dev/ttyUSB0',57600)
 
+def format_serial_package(input_array):
+    if len(input_array) == 7:
+        return Package(int.from_bytes(input_array[4], byteorder='little'), int.from_bytes(input_array[3], byteorder='little'), int.from_bytes(input_array[6], byteorder='little'))
+    else:
+        raise InvalidArgError('Input to format_serial_package was not array of length 7')
+
+def read_x_bytes(ser, data, x):
+	i = 0
+	while True:
+		if ser.inWaiting() >= 1:
+			data.append(ser.read())
+			i = i + 1
+			if i == x:
+				return data
+
+def serial_read(ser, data):	
+	#Simulated Do-while loop that reads over initial noise
+	while True:
+		if ser.inWaiting() >= 3:
+			data.append(ser.read())
+			if data[0] == b'r' or data[0] == b'n' or data[0] == b'a':
+				print('YES')
+				break
+			else:
+				print('waiting: ' + str(ser.inWaiting()) + '      just read: ' + str(data[0]))
+				data.clear()
+		else:
+			print('Not a full message in waiting')
+			return (data, False)
+	
+	
+	#If message is not for raspberry PI, skip over it		
+	if data[0] == b'a' or data[0] == b'n':
+		while True:
+			if ser.inWaiting() >= 1:
+				data.append(int.from_bytes(ser.read(), byteorder='little'))
+				if data[1] <= 11:
+					data = read_x_bytes(ser, data, x = data[1] + 1)
+					data.clear()
+					return serial_read(ser, data)
+				else:
+					print('Implied message for Non-Raspberry, but length was incompatible: {}'.format(data[1]))
+					data.clear()
+					return serial_read(ser, data)
+	
+	if data[0] == b'r':
+		while True:
+			if ser.inWaiting() >= 1:
+				data.append(int.from_bytes(ser.read(), byteorder='little'))
+				if data[1] <= 11:
+					data = read_x_bytes(ser, data, x=data[1] + 1)
+					return (data, True)
+				else:
+					print('Implied message for Raspberry, but length was incompatible: {}'.format(data[1]))
+					data.clear()
+					return serial_read(ser, data)
+	else:
+		raise ArithmeticError('First byte does not indicate proper message')
+		
+def serial_write_push(ser, bin_id):
+	ser.write(b'a')
+	ser.write((1).to_bytes(1, byteorder='little'))
+	ser.write(b'p')
+	ser.write(((bin_id % 2) + 1).to_bytes(1, byteorder='little'))
 
 #########   Bluetooth stuff  ############
 bluetooth_scale = 1
@@ -54,6 +118,13 @@ def make_client(server_socket):
     print('Accepting connection from {clientInfo}'.format(clientInfo=client_info))
     return client 
 
+	
+	
+################################################                  #####  #####    #    ###    #####
+#											   #                  #        #     # #   #  #     #
+#               BIG PYTHON MAIN                #                  #####    #    #####  ###      #
+#											   #	                  #    #    #   #  #  #     #
+################################################                  #####    #    #   #  #   #    #
 s = setup_server()
 client = make_client(s)	
 	
@@ -66,10 +137,17 @@ pa = PackingAdvisor(bin1)
 client.send(bluetooth_format_bin(pa.bins[0])
 client.send(bluetooth_format_bin(pa.bins_foreign[0])
 
+data = []
+
 while True:
-	input_string = ser.readline()
-	package = format_serial_package(input_string) #####Make this function#####
-	if pa.handle(package):
-		client.send(bluetooth_format_bin(pa.find_bin_containing_package(package)))
-	ser.sendline(str(pa.find_bin_containing_package(package).bin_id % 2))
-	client.send(bluetooth_format_package(package))
+	input_serial = serial_read(ser, data)
+	if input_serial[1]:
+		print('Package received')
+		p1 = format_serial_package(input_serial[0])
+		if pa.handle(p1):
+			client.send(bluetooth_format_bin(pa.find_bin_containing_package(p1)))
+		print(((pa.find_bin_containing_package(p1).bin_id % 2) + 1).to_bytes(1, byteorder='little'))
+		serial_write_push(ser, pa.find_bin_containing_package(p1).bin_id)
+		client.send(bluetooth_format_package(package))
+	data.clear()
+	time.sleep(1)
