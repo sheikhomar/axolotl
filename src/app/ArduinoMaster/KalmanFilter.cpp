023ca528ab1ed6d32aba4292ba17fcc28933f042
MarcBeuchert <marcbeuchert@hotmail.com>
2016-11-14 13:46:57 +0100

    A few changes to KalmanFilter.cpp

--- src/app/ArduinoMaster/KalmanFilter.cpp
+++ src/app/ArduinoMaster/KalmanFilter.cpp
@@ -11,24 +11,74 @@
 void runKalmanScheduler() {
 	KalmanFilterInformation kalmanFilterInfo;
 	double measurement = 0;
+	short falseCount = 0;
+	short count = 0;
+	bool constantReadingMode = false;
+	bool running = false;
+	bool first = true;
 
 	kalmanFilterInfo.kalmanGain = 0;
-	kalmanFilterInfo.errorInEstimate = 1000;
+	kalmanFilterInfo.errorInEstimate = 300;
 	kalmanFilterInfo.previousErrorInEstimate = 0;
-	kalmanFilterInfo.errorInMeasurement = 500;
-	kalmanFilterInfo.currentEstimate = 5000;
+	kalmanFilterInfo.errorInMeasurement = 100;
+	kalmanFilterInfo.currentEstimate = 3200;
 	kalmanFilterInfo.previousEstimate = 0;
 
 	while (true) {
 		delay(5);
-		measurement = (double)GetUltDistance(ULT1_TRIG_PIN, ULT1_ECHO_PIN, false);
-		calculateKalmanGain(&kalmanFilterInfo, measurement);
-		calculateKalmanEstimate(&kalmanFilterInfo, measurement);
-		calculateKalmanErrorInEstimate(&kalmanFilterInfo);
-		serialDebug("Measured distance: " + String(measurement));
-		serialDebug(" Kalman Gain: " + String(kalmanFilterInfo.kalmanGain));
-		serialDebug(" Kalman Estimate: " + String(kalmanFilterInfo.currentEstimate));
-		serialDebugLN(" Error in Estimate: " + String(kalmanFilterInfo.errorInEstimate));
+		measurement = (double) GetUltDistance(ULT1_TRIG_PIN, ULT1_ECHO_PIN, false);
+
+		if (measurement <= 3800 || running || constantReadingMode) {
+			count += 1;
+
+			running = true;
+			first = true;
+			if (running && !(measurement <= 3800)) {
+				falseCount += 1;
+				//serialDebug("Kalman Estimate: " + String(kalmanFilterInfo.currentEstimate));
+				//serialDebugLN(" Error in Estimate: " + String(kalmanFilterInfo.errorInEstimate));
+				//serialDebugLN(" falseCount: " + String(falseCount));
+				if (!constantReadingMode) {
+					if (falseCount == 3)
+						running = false;
+				}
+			}
+			else
+				falseCount = 0;
+
+			calculateKalmanGain(&kalmanFilterInfo, measurement);
+			calculateKalmanEstimate(&kalmanFilterInfo, measurement);
+			calculateKalmanErrorInEstimate(&kalmanFilterInfo);
+			
+			if (constantReadingMode) {
+				serialDebug("Measured distance: " + String(measurement));
+				//serialDebug(" Kalman Gain: " + String(kalmanFilterInfo.kalmanGain));
+				serialDebug(" Kalman Estimate: " + String(kalmanFilterInfo.currentEstimate));
+				serialDebugLN(" Error in Estimate: " + String(kalmanFilterInfo.errorInEstimate));
+			}
+		}
+		else {
+			if (first && count > 20) {
+				serialDebugLN("========= DONE ==========");
+				serialDebug("Kalman Estimate: " + String(kalmanFilterInfo.currentEstimate));
+				serialDebug(" Error in Estimate: " + String(kalmanFilterInfo.errorInEstimate));
+				serialDebug(" falseCount: " + String(falseCount));
+				short height = 4769.29 - kalmanFilterInfo.currentEstimate;
+				serialDebug(" Height: " + String(height));
+				serialDebugLN(" count: " + String(count) + "\n\n");
+				
+			}
+			kalmanFilterInfo.kalmanGain = 0;
+			kalmanFilterInfo.errorInEstimate = 300;
+			kalmanFilterInfo.previousErrorInEstimate = 0;
+			kalmanFilterInfo.errorInMeasurement = 100;
+			kalmanFilterInfo.currentEstimate = 3200;
+			kalmanFilterInfo.previousEstimate = 0;
+
+			count = 0;
+			falseCount = 0;
+			first = false;
+		}
 	}
 }
 
