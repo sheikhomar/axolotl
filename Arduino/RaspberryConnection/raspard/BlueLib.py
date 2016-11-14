932bcd4a1cc97ce6e6240d02cf370362a5a905bf
Frederik Kirk Kristensen <Frederik_KKristensen@hotmail.com>
2016-11-14 09:41:47 +0100

    Added classes for communication methods

---
+++ Arduino/RaspberryConnection/raspard/BlueLib.py
@@ -0,0 +1,46 @@
+import bluetooth
+
+class BlueLib:
+	def __init__(self):
+		# The MAC address of a Bluetooth adapter on the server.
+		# The server might have multiple Bluetooth adapters.
+		self.MAC_ADDR = '00:0C:78:33:A5:63'
+		self.PORT = 0 #
+		self.BACKLOG = 3
+		self.BUFFER_SIZE = 1024
+		self.SERVICE_ID = "c3ffbcc2-ab89-4e56-94ed-2a8df65e45bd"
+	
+	def bluetooth_format_package(self, package, p_advisor):
+		fragile = 0
+	
+		if package.is_fragile:
+			fragile = 1
+		if package.position is not None:
+			return 'P: {x} {y} {length} {width} {height} {colour} {fragile} {layer} {bin_id}\n'.format(x=package.position.x, y=package.position.y, length=package.length, width=package.width, height=package.height, colour=package.colour, fragile=fragile, layer=package.position.layer.find_layer_number(), bin_id=p_advisor.find_bin_containing_package(package).bin_id)
+		else:
+			return 'P: error - position not set'
+	
+	def bluetooth_format_bin(self, bin):
+		return 'B: {x} {y} {max_layers} {bin_id}\n'.format(x=bin.length, y=bin.width, max_layers=bin.max_layers, bin_id=bin.bin_id)
+		
+	def setup_server(self):
+		s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+		s.bind((self.MAC_ADDR, self.PORT))
+		s.listen(self.BACKLOG)
+		bluetooth.advertise_service(s, "PiServer Service", self.SERVICE_ID)
+		print('Server startet {mac_addr}...'.format(mac_addr=self.MAC_ADDR))
+		return s
+	
+	def handle_client(self, client):      ###Reuse this code to exception handle the BT communication: Currently not used in BigPythonMain
+		try:
+			client.send("B: 500 250 3 \n")
+			client.send("P: 0 0 100 150 1 1 \n")
+		except EOFError:
+			client.close()
+		except bluetooth.btcommon.BluetoothError:
+			client.close()
+	
+	def make_client(self, server_socket):
+		client, client_info = server_socket.accept()
+		print('Accepting connection from {clientInfo}'.format(clientInfo=client_info))
+		return client 
---
+++ Arduino/RaspberryConnection/raspard/SerLib.py
@@ -0,0 +1,75 @@
+import serial
+
+class SerLib:
+	def __init__(self, baudrate=57600):
+		self.baudrate = baudrate
+
+	def format_serial_package(self, input_array):
+		if len(input_array) == 7:
+			return Package(width=int.from_bytes(input_array[4], byteorder='little'), length=int.from_bytes(input_array[3], byteorder='little'), colour=int.from_bytes(input_array[6], byteorder='little'))
+		else:
+			raise InvalidArgError('Input to format_serial_package was not array of length 7')
+	
+	def read_x_bytes(self, ser, data, x):
+		i = 0
+		while True:
+			if ser.inWaiting() >= 1:
+				data.append(ser.read())
+				i = i + 1
+				if i == x:
+					return data
+	
+	def serial_read(self, ser, data):	
+		#Simulated Do-while loop that reads over initial noise
+		while True:
+			if ser.inWaiting() >= 3:
+				data.append(ser.read())
+				if data[0] == b'r' or data[0] == b'n' or data[0] == b'a':
+					print('Valid start char accepted')
+					break
+				else:
+					print('waiting: ' + str(ser.inWaiting()) + '      just read: ' + str(data[0]))
+					data.clear()
+			else:
+				#print('Not a full message in waiting')
+				return (data, False)
+		
+		
+		#If message is not for raspberry PI, skip over it		
+		if data[0] == b'a' or data[0] == b'n':
+			while True:
+				if ser.inWaiting() >= 1:
+					data.append(int.from_bytes(ser.read(), byteorder='little'))
+					if data[1] <= 11:
+						data = read_x_bytes(ser, data, x = data[1] + 1)
+						data.clear()
+						return serial_read(ser, data)
+					else:
+						print('Implied message for Non-Raspberry, but length was incompatible: {}'.format(data[1]))
+						data.clear()
+						return serial_read(ser, data)
+		
+		if data[0] == b'r':
+			while True:
+				if ser.inWaiting() >= 1:
+					data.append(int.from_bytes(ser.read(), byteorder='little'))
+					if data[1] <= 11:
+						data = read_x_bytes(ser, data, x=data[1] + 1)
+						return (data, True)
+					else:
+						print('Implied message for Raspberry, but length was incompatible: {}'.format(data[1]))
+						data.clear()
+						return serial_read(ser, data)
+		else:
+			raise ArithmeticError('First byte does not indicate proper message')
+			
+	def serial_write_push(self, ser, bin_id):
+		ser.write(b'a')
+		ser.write((1).to_bytes(1, byteorder='little'))
+		ser.write(b'p')
+		ser.write(((bin_id % 2) + 1).to_bytes(1, byteorder='little'))
+		print('Just send bin_id: {bin_id}'.format(bin_id=bin_id))
+		
+	def make_serial(self):
+		serial_instance = serial.Serial('/dev/ttyUSB0', self.baudrate)
+		return serial_instance
