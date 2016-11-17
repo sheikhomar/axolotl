639b265d020bb355b74a9387c46e1e1014b6538f
Frederik Kirk Kristensen <Frederik_KKristensen@hotmail.com>
2016-11-17 09:17:30 +0100

    BPThreadM: Now actually utilises locks + more threads

--- Arduino/RaspberryConnection/raspard/BigPythonThreadMain.py
+++ Arduino/RaspberryConnection/raspard/BigPythonThreadMain.py
@@ -26,10 +26,11 @@ BTReady = False
 
 bin_buf = []
 pack_buf = []
+thread_number = 2
 
 def make_client_thread(server_input):
-	global BTReady, client #Tell interpreter to use global BTReady instead of creating new local
-	print('Thread 2 started')
+	global BTReady, client, thread_number #Tell interpreter to use global BTReady instead of creating new local
+	print('Thread {number} started'.format(number=thread_number))
 	client = BLib.make_client(server_input)
 	bluetooth_lock.acquire()
 	for b in bin_buf:
@@ -37,8 +38,10 @@ def make_client_thread(server_input):
 	for p in pack_buf:
 		client.send(BLib.bluetooth_format_package(p, pa))
 	BTReady = True
-	print('Thread 2 is done')
+	print('Thread {number} is done'.format(number=thread_number))
 	bluetooth_lock.release()
+	thread_number = thread_number + 1
+	_thread.start_new_thread(make_client_thread, (server_input,)) #Start another thread to handle next bluetooth connection
 
 
 
@@ -61,17 +64,21 @@ while True: #THE REAL DEAL
 	if input_serial[1]:
 		print('Message received')
 		if input_serial[0][2] == b'p':
+			bluetooth_lock.acquire()
 			p1 = SLib.format_serial_package(input_serial[0])
 			if pa.handle(p1):
 				if BTReady:
 					client.send(BLib.bluetooth_format_bin(pa.find_bin_containing_package(p1)))
+					bin_buf.append(pa.find_bin_containing_package(p1))
 				else:
 					bin_buf.append(pa.find_bin_containing_package(p1))
 			SLib.serial_write_push(ser, pa.find_bin_containing_package(p1).bin_id)
 			if BTReady:
 				client.send(BLib.bluetooth_format_package(p1, pa))
+				pack_buf.append(p1)
 			else:
 				pack_buf.append(p1)
+			bluetooth_lock.release()
 		elif input_serial[0][2] == b'R':
 			SLib.serial_write_last_push(ser)
 	data.clear()
