74514709b0aa0a9304508351959a9d2913173b60
Omar Ali Sheikh-Omar <sheikhomar@gmail.com>
2016-12-04 13:52:57 +0100

    Adjust saturation on different colours.

--- src/app/Presentation/app/src/main/java/com/axolotl/presentation/model/PackageColour.java
+++ src/app/Presentation/app/src/main/java/com/axolotl/presentation/model/PackageColour.java
@@ -8,9 +8,12 @@ public class PackageColour {
     private final int paintColour;
     private final String name;
     private static PackageColour[] Colours = {
-            new PackageColour(0, Color.RED, "Red"),
-            new PackageColour(1, Color.BLUE, "Blue"),
-            new PackageColour(2, Color.GREEN, "Green"),
+            new PackageColour(0, Color.argb(255, 214, 24, 0), "Red"),
+            new PackageColour(1, Color.argb(255, 0, 72, 181), "Blue"),
+            new PackageColour(2, Color.argb(255, 0, 160, 18), "Green"),
+            new PackageColour(3, Color.argb(255, 237, 197, 0), "Yellow"),
+            new PackageColour(6, Color.WHITE, "White"),
+            new PackageColour(7, Color.BLACK, "Black"),
             new PackageColour(128, Color.BLACK, "Unknown"),
     };
 
