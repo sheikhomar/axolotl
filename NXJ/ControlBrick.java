86b5f5c5c4f8a8deaac20c598b6e6baa1086d5c7
stelle <stellehj@yahoo.dk>
2016-12-06 15:01:44 +0100

    Updated ControlBrick + added comments
    New impl of colour sensor reading, now reading continuously and saving
    in a list. The entire code is now commented

--- NXJ/ControlBrick.java
+++ NXJ/ControlBrick.java
@@ -1,8 +1,9 @@
 import lejos.nxt.*;
 import lejos.util.*;
 import lejos.nxt.comm.RS485;
+import java.util.*;
 
-public class ControlBrick {
+public class ControlBrick {	
 	private static NXTMotor ma = new NXTMotor(MotorPort.A);
 	private static NXTMotor mb = new NXTMotor(MotorPort.B);
 	private static NXTMotor mc = new NXTMotor(MotorPort.C);
@@ -10,6 +11,18 @@ public class ControlBrick {
 	private static final int DATASIZE = 11;
 	private static Stopwatch stopwatch = new Stopwatch();
 	private static boolean beltRunning = false;
+	private static List<Integer> colourList = new ArrayList<Integer>();
+	private static boolean readyForNewBlock = true;
+	private static int badColourCount = 0;
+	private static int previousColour = 128;
+	private static int goodColourCount = 0;
+	private static int listResetCount = 0;
+	private static final int RED = 0, GREEN = 1, BLUE = 2, YELLOW = 3, WHITE = 6, BLACK = 7, NOCOLOUR = 127;
+	private static final String REDSTRING = "Red", GREENSTRING = "Green", BLUESTRING = "Blue", YELLOWSTRING = "Yellow", 
+								WHITESTRING = "White", BLACKSTRING = "Black", NOCOLOURSTRING = "NoColour";
+	private static final String AARM = "A", BARM = "B", CBELTSTART = "CStart", CBELTSTOP = "CStop", CBELTSPEED = "CSpeed", 
+								CSENSORREAD = "ReadColour", CSENSORSEND = "SendColour", CSENSORTEST = "ColourTest", DEFAULTPROGRAM = "unknown program";
+	private static final int MIN_GOODCOLOURCOUNT = 2, MIN_BADCOLOURCOUNT = 5;
 
     public static void main(String[] args) throws InterruptedException {
 		System.out.println("A-mei-zing!");
@@ -21,6 +34,7 @@ public class ControlBrick {
 		
 		while(true){
 			buttonFuncs();
+			control(CSENSORREAD);
 			readInput(true);
 			if(recBuff[0] == 110){ //Checking if NXJ should do something or not -- 110 = 'n'
 				recString = determineRecString();
@@ -30,17 +44,19 @@ public class ControlBrick {
 			else if(recBuff[0] == 97 || recBuff[0] == 114 || recBuff[0] == 32){ //97 = 'a', 114 = 'r', 32 = ' '
 				skipInput();
 			}
+			checkToClearColourList();
 		}
     }
 	
 	private static String ConvertColorEnum(int input) {
 		switch(input) {
-		case 0: return "Red";
-		case 1: return "Green";
-		case 2: return "Blue"; 
-		case 3: return "Yellow";
-		case 6: return "White";
-		case 7: return "Black";
+		case RED: return REDSTRING;
+		case GREEN: return GREENSTRING;
+		case BLUE: return BLUESTRING; 
+		case YELLOW: return YELLOWSTRING;
+		case WHITE: return WHITESTRING;
+		case BLACK: return BLACKSTRING;
+		case NOCOLOUR: return NOCOLOURSTRING;
 		default: return "Irrelevant";
 		}
 	}
@@ -50,15 +66,14 @@ public class ControlBrick {
 		if(recBuff[0] < DATASIZE + 1){ //OBS. See contract if confused -- length should be legal
 			readInput();
 			switch(recBuff[0]){ //Function to be performed
-				case 0: return "A";
-				case 1: return "B";
-				case 2: return "AB";
-				case 3: return "CStart";
-				case 4: return "CStop";
-				case 5: return "Colour";
-				case 6: return "CSpeed";
-				case 11: return "ColourTest";
-				default: return "unknown program";
+				case 0: return AARM;
+				case 1: return BARM;
+				case 3: return CBELTSTART;
+				case 4: return CBELTSTOP;
+				case 5: return CSENSORSEND;
+				case 6: return CBELTSPEED;
+				case 11: return CSENSORTEST;
+				default: return DEFAULTPROGRAM;
 			}
 		}
 		else{
@@ -68,28 +83,28 @@ public class ControlBrick {
 	
 	private static void control(String input){ //Running correct function
 		switch(input){
-			case "A":
+			case AARM:
 				backAndForthA();
 				break;
-			case "B":
+			case BARM:
 				backAndForthB();
 				break;
-			case "CStart":
+			case CBELTSTART:
 				fullSpeedC();
 				break;
-			case "CStop":
+			case CBELTSTOP:
 				stopC();
 				break;
-			case "AB":
-				backAndForthAB();
+			case CSENSORREAD:
+				readColour();
 				break;
-			case "Colour":
-				readAndSendColour();
+			case CSENSORSEND:
+				sendColour();
 				break;
-			case "CSpeed":
+			case CBELTSPEED:
 				adjustSpeedC();
 				break;
-			case "ColourTest":
+			case CSENSORTEST:
 				readAndSendBrickColour();
 				break;
 			default:
@@ -97,83 +112,69 @@ public class ControlBrick {
 		}
 	}
 	
-	private static void readAndSendColour(){ //Reading colour and sending it to Ardu no matter the result
+	private static void readColour(){ //Reading colour
 		ColorSensor sensor = new ColorSensor(SensorPort.S1);
-		int succesful = 0, colour = -1;
+		int colour = NOCOLOUR;
+
+		colour = sensor.getColorID();
+		Delay.msDelay(7); //Can't read colour sensor to often so we need a small delay
+		if(readyForNewBlock){
+			if(colour == RED || colour == GREEN || colour == BLUE || colour == YELLOW){ //Check if read colour is valid
+				if(previousColour == colour){ //previousColour is NOCOLOUR as default -- here we're checking if previous colour read is the same as the current read colour
+					goodColourCount++;
+				}
+				else{ //If we just read another valid colour than we should e.g. the brick is blue and we read a green instead
+					previousColour = colour;
+					goodColourCount = 0;
+				}
+				
+				if(goodColourCount >= MIN_GOODCOLOURCOUNT){ //We need to read the same colour MIN_GOODCOLOURCOUNT(2) in a row before we conclude it's the true colour of the brick
+					colourList.add(colour);
+					readyForNewBlock = false;
+					badColourCount = 0;
+					previousColour = NOCOLOUR;
+					goodColourCount = 0;
+				}
+				
+				listResetCount = stopwatch.elapsed();
+			}
+		}
+		else if(colour == WHITE || colour == BLACK){ //We want to read at least MIN_BADCOLOURCOUNT(5) black or white colours between each brick before we conclude we are ready for the next brick
+			if(badColourCount >= MIN_BADCOLOURCOUNT){
+				readyForNewBlock = true;
+			}
+			badColourCount++;
+		}
+	}
+	
+	private static void sendColour(){ //Sending colour to Ardu
+		int succesful = 0, tempColour = NOCOLOUR;
 		byte[] sendBuff = new byte[4];
 		sendBuff[0] = 'a';
 		sendBuff[1] = 1;
 		sendBuff[2] = 1;
 		
-		colour = sensor.getColorID();
-		sendBuff[3] = (byte)colour;
+		if(!colourList.isEmpty()){
+			tempColour = colourList.remove(0);
+			sendBuff[3] = (byte)tempColour;
+		}
+		else{
+			sendBuff[3] = (byte)NOCOLOUR; //Default colour send to arduino if the list is empty (the package is removed from arudino)
+		}
+		
 		while(succesful == 0){
 			succesful = RS485.hsWrite(sendBuff, 0, sendBuff.length); //hsWrite --> Returning number of bytes send
 		}
-		System.out.println("Sending " + ConvertColorEnum(colour));
+		
+		System.out.println("Sending " + ConvertColorEnum(((int)sendBuff[3])));
 	}
 	
 	private static void backAndForthA(){
-		ma.setPower(100);
-		int count = 0;
-		
-		while(count < 2){
-			ma.stop();
-			if(count % 2 == 0){
-				ma.backward();
-			}
-			else{
-				ma.forward();
-			}
-			Delay.msDelay(180);
-			count++;
-		}
-		
-		ma.stop();
-		//Power: 50		Delay:380
-		//Power: 75		Delay:240
-		//Power: 100	Delay:180
+		executeOnePush(ma); //Running in a thread making the program able to do other stuff (there's two delays for 180 each making this preferable)
 	}
 	
 	private static void backAndForthB(){
-		mb.setPower(100);
-		int count = 0;
-		
-		while(count < 2){
-			mb.stop();
-			if(count % 2 == 0){
-				mb.backward();
-			}
-			else{
-				mb.forward();
-			}
-			Delay.msDelay(180);
-			count++;
-		}
-		
-		mb.stop();
-	}
-	
-	private static void backAndForthAB(){
-		ma.setPower(100);
-		mb.setPower(100);
-		int count = 0;
-		
-		while(count < 2){
-			if(count % 2 == 0){
-				ma.backward();
-				mb.backward();
-			}
-			else{
-				ma.forward();
-				mb.forward();
-			}
-			Delay.msDelay(180);
-			count++;
-		}
-		
-		ma.stop();
-		mb.stop();
+		executeOnePush(mb);
 	}
 	
 	private static void fullSpeedC(){
@@ -197,7 +198,7 @@ public class ControlBrick {
 		}
 	}
 	
-	private static void readInput(){ //When calling readInput the default is no parameter which result in no timeout
+	private static void readInput(){ //When calling readInput the default is no parameter which result in no timeout and no msg
 		readInput(false);
 	}
 	
@@ -209,7 +210,7 @@ public class ControlBrick {
 		int succesful = 0;
 		while(succesful == 0){
 			succesful = RS485.hsRead(recBuff, 0, recBuff.length);
-			if(timeLimitActive && maxReadingTimeReached(enableMsg)){
+			if(timeLimitActive && maxReadingTimeReached(enableMsg)){ //maxReadingTimeReached is only called when skipInput() is called -- shortcircuit
 				break;
 			}
 		}
@@ -267,15 +268,23 @@ public class ControlBrick {
 		}
 	}
 	
+	private static void checkToClearColourList(){ //Clear the colourList if more than 1 sec since last brick is read
+		final int MAX_READING_TIME = 1000;
+		if(stopwatch.elapsed() - listResetCount > MAX_READING_TIME){
+			colourList.clear();
+			listResetCount = stopwatch.elapsed();
+		}
+	}
+	
 	private static void readAndSendBrickColour(){ // For test purpose only, reads colour untill a valid brick colour is read -- can livelock system
 		ColorSensor sensor = new ColorSensor(SensorPort.S1);
-		int succesful = 0, colour = -1;
+		int succesful = 0, colour = NOCOLOUR;
 		byte[] sendBuff = new byte[4];
 		sendBuff[0] = 'a';
 		sendBuff[1] = 1;
 		sendBuff[2] = 1;
 		
-		while (!(colour == 0 || colour == 1 || colour == 2 || colour == 3)) { //0 = Red, 1 = Green, 2 = Blue, 3 = Yellow
+		while (!(colour == RED || colour == GREEN || colour == BLUE || colour == YELLOW)) { //0 = Red, 1 = Green, 2 = Blue, 3 = Yellow
 			colour = sensor.getColorID();
 		}
 		sendBuff[3] = (byte)colour;
@@ -283,5 +292,32 @@ public class ControlBrick {
 			succesful = RS485.hsWrite(sendBuff, 0, sendBuff.length); //Sending untill 0 < bytes are send
 		}
 		System.out.println("Sending " + ConvertColorEnum(colour));
+	}
+	
+	private static void executeOnePush(NXTMotor m){ //Running thread where a given motor is performing a push to avoid stalling the system in the delays
+		Thread thread = new Thread(){
+			public void run(){
+				m.setPower(100);
+				int count = 0;
+				
+				while(count < 2){
+					m.stop();
+					if(count % 2 == 0){
+						m.backward();
+					}
+					else{
+						m.forward();
+					}
+					Delay.msDelay(180);
+					count++;
+				}
+				
+				m.stop();
+				//Power: 50		Delay:360
+				//Power: 75		Delay:240
+				//Power: 100	Delay:180
+			}
+		};
+		thread.start();
 	}
 }
