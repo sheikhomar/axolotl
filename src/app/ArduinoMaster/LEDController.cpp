a5bcc12f15ba7fd12fae1f9742c12a0a111659f2
MBS <mathiasbsvendsen@gmail.com>
2016-12-06 13:27:11 +0100

    Led controller v2

--- src/app/ArduinoMaster/LEDController.cpp
+++ src/app/ArduinoMaster/LEDController.cpp
@@ -4,7 +4,7 @@
 
 #include "LEDController.h"
 
-void ledSetup(long ledA, long ledB, long ledC) {
+void ledSetup(unsigned long ledA, unsigned long ledB, unsigned long ledC) {
 	pinMode(ledA, OUTPUT);
 	pinMode(ledB, OUTPUT);
 	pinMode(ledC, OUTPUT);
@@ -14,6 +14,72 @@ void ledSetup(long ledA, long ledB, long ledC) {
 	digitalWrite(ledC, LOW);
 }
 
-void led(int LED, bool TurnOn) {
+
+void led(unsigned long LED, bool TurnOn) {
 	digitalWrite(LED, TurnOn);
+}
+
+
+void initLEDController(LedState *state, unsigned long statusLED, unsigned long cycleLED, unsigned long errorLED,
+						unsigned long statusLEDTime = STATUS_TIME_CYCLES, unsigned long cycleLEDHalfTime = CYCLE_HALFTIME_CYCLES, 
+						unsigned long errorLEDTime = ERROR_TIME_CYCLES) {
+
+	state->statusLED = statusLED;
+	state->cycleLED = cycleLED;
+	state->errorLED = errorLED;
+	state->statusLEDTime = statusLEDTime;
+	state->cycleLEDHalfTime = cycleLEDHalfTime;
+	state->errorLEDTime = errorLEDTime;
+
+}
+
+void updateLED(LedState *state) {
+	unsigned long halfCycle = state->cycleLEDHalfTime;
+	unsigned long fullCycle = halfCycle * 2;
+
+	//Update cycle count
+	state->cycleCount = ((state->cycleCount) % fullCycle) + 1;
+
+	//Update cycle timer
+	if (state->cycleCount == halfCycle) {
+		led(state->cycleLED, true);
+		Serial.print("#");
+	}
+	else if (state->cycleCount == fullCycle) {
+		led(state->cycleLED, false);
+		Serial.print("-");
+	}
+	else {
+		Serial.print(".");
+	}
+
+	//Count status lamp
+	if (state->statusCount > 0) {
+		state->statusCount -= 1;
+
+		if (state->statusCount == 0) {
+			led(state->statusLED, false);
+		}
+	}
+
+	//Count debug lamp
+	if (state->errorCount > 0) {
+		state->errorCount -= 1;
+
+		if (state->errorCount == 0) {
+			led(state->errorLED, false);
+		}
+	}
+
+
+}
+
+void statusLamp(LedState *state) {
+	state->statusCount = state->statusLEDTime;
+	led(state->statusLED, true);
+}
+
+void errorLamp(LedState *state) {
+	state->errorCount = state->errorLEDTime;
+	led(state->errorLED, true);
 }
--- src/app/ArduinoMaster/LEDController.h
+++ src/app/ArduinoMaster/LEDController.h
@@ -9,10 +9,34 @@
 	#include "WProgram.h"
 #endif
 
-//Functions
-void ledSetup(long ledA, long ledB, long ledC);
-void led(int LED, bool TurnOn);
+//Defines
+#include "Defines.h"
+#define STATUS_TIME_CYCLES 100
+#define CYCLE_HALFTIME_CYCLES 25
+#define ERROR_TIME_CYCLES 100
 
+//Struct
+typedef struct LedState {
+	unsigned long statusCount = 0;
+	unsigned long cycleCount = 0;
+	unsigned long errorCount = 0;
+	unsigned long statusLED = 0;
+	unsigned long cycleLED = 0; 
+	unsigned long errorLED = 0;
+	unsigned int statusLEDTime = STATUS_TIME_CYCLES;
+	unsigned int cycleLEDHalfTime = CYCLE_HALFTIME_CYCLES;
+	unsigned int errorLEDTime = ERROR_TIME_CYCLES;
+} LedState;
+
+//Functions
+void ledSetup(unsigned long ledA, unsigned long ledB, unsigned long ledC);
+void led(unsigned long LED, bool TurnOn);
+void updateLED(LedState *state);
+void statusLamp(LedState *state);
+void errorLamp(LedState *state);
+void initLEDController(LedState *state, unsigned long statusLED, unsigned long cycleLED, unsigned long errorLED,
+	unsigned long statusLEDTime = STATUS_TIME_CYCLES, unsigned long cycleLEDHalfTime = CYCLE_HALFTIME_CYCLES,
+	unsigned long errorLEDTime = ERROR_TIME_CYCLES);
 
 #endif
 
