185da12e7f528b1644dd37d56592d626824c35d7
Frederik Kirk Kristensen <Frederik_KKristensen@hotmail.com>
2016-12-07 13:33:58 +0100

    PYMain: Removed the Bluetooth crash bug :cactus:

--- Arduino/RaspberryConnection/raspard/BigPythonThreadMain.py
+++ Arduino/RaspberryConnection/raspard/BigPythonThreadMain.py
@@ -1,5 +1,6 @@
 import time
 import _thread
+import bluetooth
 
 
 from lib import *
@@ -18,7 +19,19 @@ ser = SLib.make_serial()
 #########   Bluetooth stuff  ############
 BLib = BlueLib()
 s = BLib.setup_server()
-client = None #Client is initially set to None and is later created in thread
+clients = [] #Client is initially set to empty and is later appended in thread
+
+def try_bluetooth_send(client_list, message):
+	#print('Number of clients: {clientnumber}'.format(clientnumber=len(client_list)))
+	for x in range(len(client_list)-1, -1, -1):
+		try:
+			print('And he tries. Oh my god does he try')
+			client_list[x].send(message)
+		except bluetooth.BluetoothError:
+			print('BluetoothError excepted and client removed')
+			client_list.pop(x)
+	#print('Number of clients after: {clientnumber}'.format(clientnumber=len(client_list)))
+		
 
 ###### Threading stuff #######
 bluetooth_lock = _thread.allocate_lock()
@@ -29,14 +42,15 @@ pack_buf = []
 thread_number = 2
 
 def make_client_thread(server_input):
-	global BTReady, client, thread_number #Tell interpreter to use global BTReady instead of creating new local
+	global BTReady, clients, thread_number #Tell interpreter to use global BTReady instead of creating new local
 	print('Thread {number} started'.format(number=thread_number))
-	client = BLib.make_client(server_input)
+	client_local = BLib.make_client(server_input)
 	bluetooth_lock.acquire()
+	clients.append(client_local)
 	for b in bin_buf:
-		client.send(BLib.bluetooth_format_bin(b))
+		client_local.send(BLib.bluetooth_format_bin(b))
 	for p in pack_buf:
-		client.send(BLib.bluetooth_format_package(p, pa))
+		client_local.send(BLib.bluetooth_format_package(p, pa))
 	BTReady = True
 	print('Thread {number} is done'.format(number=thread_number))
 	bluetooth_lock.release()
@@ -68,18 +82,16 @@ while True: #THE REAL DEAL
 			p1 = SLib.format_serial_package(input_serial[0])
 			if pa.handle(p1):
 				if BTReady:
-					client.send(BLib.bluetooth_format_bin(pa.find_bin_containing_package(p1)))
+					try_bluetooth_send(clients, BLib.bluetooth_format_bin(pa.find_bin_containing_package(p1)))
 					bin_buf.append(pa.find_bin_containing_package(p1))
 				else:
 					bin_buf.append(pa.find_bin_containing_package(p1))
 			SLib.serial_write_push(ser, pa.find_bin_containing_package(p1).bin_id)
 			if BTReady:
-				client.send(BLib.bluetooth_format_package(p1, pa))
+				try_bluetooth_send(clients, BLib.bluetooth_format_package(p1, pa))
 				pack_buf.append(p1)
 			else:
 				pack_buf.append(p1)
 			bluetooth_lock.release()
-		elif input_serial[0][2] == b'R':
-			SLib.serial_write_last_push(ser)
 	data.clear()
 	#time.sleep(1)
