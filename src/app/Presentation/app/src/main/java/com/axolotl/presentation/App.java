0db555d17cbca7440d45a0115d311c39af8538bf
Omar Ali Sheikh-Omar <sheikhomar@gmail.com>
2016-12-07 15:19:54 +0100

    Fix sample to use new packet format.

--- src/app/Presentation/app/src/main/java/com/axolotl/presentation/App.java
+++ src/app/Presentation/app/src/main/java/com/axolotl/presentation/App.java
@@ -2,6 +2,8 @@ package com.axolotl.presentation;
 
 import android.app.Application;
 
+import com.axolotl.presentation.communication.CommandTranslator;
+import com.axolotl.presentation.communication.InvalidCommandException;
 import com.axolotl.presentation.model.Repository;
 
 public class App extends Application {
@@ -24,35 +26,35 @@ public class App extends Application {
             cm.translate("B: 10 8 3 1 Foreign", repository);
 
             // Pack packages into layer 1
-            cm.translate("P: 0 0 2 2 1 0 0 1 1", repository);
-            cm.translate("P: 2 0 2 2 1 1 0 1 1", repository);
-            cm.translate("P: 4 0 2 2 1 2 0 1 1", repository);
-            cm.translate("P: 6 0 2 2 1 3 0 1 1", repository);
-            cm.translate("P: 0 2 2 2 1 6 0 1 1", repository);
-            cm.translate("P: 2 2 2 2 1 7 0 1 1", repository);
-            cm.translate("P: 4 2 2 2 1 128 0 1 1", repository);
+            cm.translate("P: 0 0 2 2 160 160 160 0 0 1 1", repository);
+            cm.translate("P: 2 0 2 2 160 160 160 1 0 1 1", repository);
+            cm.translate("P: 4 0 2 2 160 160 160 2 0 1 1", repository);
+            cm.translate("P: 6 0 2 2 160 160 160 3 0 1 1", repository);
+            cm.translate("P: 0 2 2 2 160 160 160 6 0 1 1", repository);
+            cm.translate("P: 2 2 2 2 160 160 160 7 0 1 1", repository);
+            cm.translate("P: 4 2 2 2 160 160 160 128 0 1 1", repository);
 
             // Pack packages into layer 2
-            cm.translate("P: 0 0 2 8 1 0 0 2 1", repository);
-            cm.translate("P: 2 0 2 4 1 1 0 2 1", repository);
-            cm.translate("P: 4 0 2 2 1 2 0 2 1", repository);
+            cm.translate("P: 0 0 2 8 160 160 160 0 0 2 1", repository);
+            cm.translate("P: 2 0 2 4 160 160 160 1 0 2 1", repository);
+            cm.translate("P: 4 0 2 2 160 160 160 2 0 2 1", repository);
 
             // Pack packages into layer 3
-            cm.translate("P: 0 0 2 8 1 2 0 3 1", repository);
-            cm.translate("P: 2 0 2 4 1 2 0 3 1", repository);
-            cm.translate("P: 4 0 2 2 1 2 0 3 1", repository);
+            cm.translate("P: 0 0 2 8 160 160 160 2 0 3 1", repository);
+            cm.translate("P: 2 0 2 4 160 160 160 2 0 3 1", repository);
+            cm.translate("P: 4 0 2 2 160 160 160 2 0 3 1", repository);
 
             // Define bin 2
             cm.translate("B: 10 8 2 2 Foreign", repository);
 
             // Pack packages into layer 1
-            cm.translate("P: 2 1 2 2 1 0 0 1 2", repository);
-            cm.translate("P: 3 3 2 2 1 1 0 1 2", repository);
-            cm.translate("P: 6 0 2 2 1 2 0 1 2", repository);
+            cm.translate("P: 2 1 2 2 160 160 160 0 0 1 2", repository);
+            cm.translate("P: 3 3 2 2 160 160 160 1 0 1 2", repository);
+            cm.translate("P: 6 0 2 2 160 160 160 2 0 1 2", repository);
 
             // Pack packages into layer 2
-            cm.translate("P: 0 0 2 8 1 0 0 2 2", repository);
-            cm.translate("P: 2 0 2 4 1 1 0 2 2", repository);
+            cm.translate("P: 0 0 2 8 160 160 160 0 0 2 2", repository);
+            cm.translate("P: 2 0 2 4 160 160 160 1 0 2 2", repository);
 
 
         } catch (InvalidCommandException e) {
