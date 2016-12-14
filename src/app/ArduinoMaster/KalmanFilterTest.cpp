cbf164dfa47bb7e6125382ddecf4498b82f6bd34
MBS <mathiasbsvendsen@gmail.com>
2016-12-14 13:33:31 +0100

    Added partial code for the kalman filter test

--- src/app/ArduinoMaster/KalmanFilterTest.cpp
+++ src/app/ArduinoMaster/KalmanFilterTest.cpp
@@ -8,18 +8,54 @@
 
 
 void KalmanFilterTestRun() {
-
+	//initKalmanFilter
+	initkalmanFilterTest();
 
 	//KalmanGain
 
+
+
+
 	//KalmanEstiamte
+
+
+
 
 	//KalamnErrorInEstimate
 
+
+
+
 	//ResetKalmanFilter
+
+
+
 
 	//update prediction
 
-	//initKalmanFilter
-
 }
+
+
+void initKalmanFilterTest() {
+	KalmanFilterInformation kfi;
+	kfi.currentEstimate = 1;
+	kfi.errorInEstimate = 2;
+	kfi.kalmanGain = 3;
+	kfi.processNoise = 4;
+	kfi.sensorNoise = 5;
+
+	double errorInEsitmate = 10, sensorNoise = 11, currentEstimate = 12, processNoise = 13;
+
+	initKalmanFilter(&kfi, errorInEsitmate, sensorNoise, currentEstimate, processNoise);
+
+	if (
+		kfi.currentEstimate != currentEstimate ||
+		kfi.errorInEstimate != errorInEsitmate ||
+		kfi.kalmanGain != 0 ||
+		kfi.processNoise != processNoise ||
+		kfi.sensorNoise != sensorNoise) {
+			serialDebugLN("KalmanFilter was not correctly initialized.");
+	}
+}
+
+void 
