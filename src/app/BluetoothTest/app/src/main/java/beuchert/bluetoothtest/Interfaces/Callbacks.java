cd299c9c61b3f8682f1292edc1c310925daa5a0f
Omar Ali Sheikh-Omar <sheikhomar@gmail.com>
2016-12-16 11:08:54 +0100

    Remove old Android project.

--- src/app/BluetoothTest/app/src/main/java/beuchert/bluetoothtest/Interfaces/Callbacks.java
+++
@@ -1,9 +0,0 @@
-package beuchert.bluetoothtest.Interfaces;
-
-public interface Callbacks {
-    void showBluetoothConnectionAlert();
-    void showBluetoothAdapterAlert();
-    void showBluetoothNotEnabledAlert();
-    void handlePackage(String Package);
-    void isServiceReady(boolean ready);
-}
